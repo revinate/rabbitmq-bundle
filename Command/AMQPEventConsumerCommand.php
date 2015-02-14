8892fde4fc96cc0d87ab9f59f0af5e8fe5bf9e15
Vinay Kumar <vinay@revinate.com>
2015-02-13 17:41:13 -0800

    moving command to top level directory otherwise it is not registered

--- Command/AMQPEventConsumerCommand.php
+++ Command/AMQPEventConsumerCommand.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Revinate\RabbitMqBundle\AMQP\Command;
+namespace Revinate\RabbitMqBundle\Command;
 
 use PhpAmqpLib\Exception\AMQPTimeoutException;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
