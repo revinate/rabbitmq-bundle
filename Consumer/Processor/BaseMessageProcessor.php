cc0a05c1f9bf671494ede15fb2ebf7f039c39ed8
jorgeloria-logn <36684222+jorgeloria-logn@users.noreply.github.com>
2018-09-04 13:21:36 -0600

    EOC-REV-17383 Added retry when acknowledging messages (#6)
    * EOC-REV-17383 Added retry when acknowledging messages
    
    * EOC-REV-17383 removed invalid error_log function

--- Consumer/Processor/BaseMessageProcessor.php
+++ Consumer/Processor/BaseMessageProcessor.php
@@ -28,6 +28,9 @@ abstract class BaseMessageProcessor {
     /** @var  Queue */
     protected $queue;
 
+    /** max retries to ack/nack */
+    const MAX_RETRY_COUNT = 100;
+
     /**
      * @param Consumer $consumer
      */
@@ -86,7 +89,20 @@ abstract class BaseMessageProcessor {
         // Ack or Nack Messages
         foreach ($messages as $index => $message) {
             $processFlag = is_array($processFlagOrFlags) && isset($processFlagOrFlags[$index]) ? $processFlagOrFlags[$index] : $processFlagOrFlags;
-            $this->consumer->ackOrNackMessage($message, $processFlag, $exception);
+            $retry = true;
+            $retryCount = 0;
+            while($retry) {
+                try {
+                    $this->consumer->ackOrNackMessage($message, $processFlag, $exception);
+                    $retry = false;
+                } catch (\Exception $e) {
+                    ++$retryCount;
+                    if($retryCount > self::MAX_RETRY_COUNT){
+                        throw $e;
+                    }
+                    usleep(25000);//0.025 seconds
+                }
+            }
         }
     }
 
